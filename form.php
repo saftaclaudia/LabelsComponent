5cfd6da7bbb84c0288af7f3a64a8bde13915666c
saftaclaudia <saftaclaudia27@yahoo.com>
2014-09-21 22:46:18 +0300

    form jQuery
    append and remove

---
+++ form.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+      <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+      <script type='text/javascript' src="script.js"> </script>
+      <body>
+        <form>
+          <input type="text" name="user_input" id="user_input">
+        </form>
+          <input type="submit" id="button" value="send">
+          <div class="add_input"></div>
+      </body>
+
+</html>
---
+++ script.js
@@ -0,0 +1,14 @@
+  $(document).ready(function(){
+          var max_input=6;
+          var x=0; 
+          $('#button').click(function(){
+            var toAdd= $('input[name=user_input]').val();
+              if(x< max_input){
+               x++;
+               $('.add_input').append('<div class="add"><a href="#">&times</a>' +toAdd+ '</div>');
+              };
+          });
+          $('a').click(function(){
+            $('.add').remove();
+          });
+        });
