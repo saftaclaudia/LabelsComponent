1243e9986b97de4e3092de3c49e9494979c8e78d
saftaclaudia <saftaclaudia27@yahoo.com>
2014-09-25 19:31:47 +0300

    information messages

--- script.js
+++ script.js
@@ -5,16 +5,28 @@ $(document).ready(function(){
     e.preventDefault();
     var toAdd= $('input[name=user_input]').val();
     var addContent= $('<div class="add"><a href="#">&times</a>' +toAdd+ '</div>');
-    if($('.add').length < max_input && toAdd.length > 0){
+    
+    if($('.add').length === max_input && $('.message_1').length<1 && toAdd.length > 0){
+      $('.add_input').prepend('<p class="message_1">text1</p>');
+    };
+
+    $('.add').each(function(element){
+      if( $('element').text() === toAdd && $('.message_1').length<1 && toAdd.length > 0){
+        $('.add_input').prepend('<p class="message_2">text2</p>');
+      };
+    });
+
+   if($('.add').length < max_input && toAdd.length > 0 && toAdd !== ""){
       $('.add_input').append(addContent);
       $('#user_input').val('');
       $('#user_input').focus();
     };
   });
-        
+       
   $('.add_input').on('click', 'a', function(e){
     e.preventDefault();
     $(this).closest('.add').remove();
   });
+
 });
  
