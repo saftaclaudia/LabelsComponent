4a897ff7f1940b971b5b75d82710fa1eaed1ffe2
saftaclaudia <saftaclaudia27@yahoo.com>
2014-09-30 21:05:46 +0300

    #15 and #16 Disable "Remove All'  and "Send" button
    Disable "Remove all" button if there is no label and Disable "send"
    button if the maximum number of label is reached.
    - Create function disableButton
    - move var displayErrorMessage   outside the click  event
    -delete message for maxim input from the click event.

--- script.js
+++ script.js
@@ -1,28 +1,45 @@
 $(document).ready(function(){
   var max_input=6; 
   $('<div class="add_input"></div>').appendTo($('form'));
-  
+
+  var displayErrorMessage = function(text) {
+    $('<p>'+ text +'</p>')
+      .hide()
+      .insertAfter($('#resetButton'))
+      .show(200);
+    };
+
+  var disableButton = function(){
+    if($('.add').length === max_input){
+      $('#button').attr('disabled', 'disabled');
+      displayErrorMessage( 'You have reached maximum capacity. Remove a label to add a new message');
+    }
+
+    else{
+      $('#button').removeAttr('disabled');
+      $('p').remove();
+    }
+
+    if($('.add').length === 0){
+      $('#resetButton').attr('disabled', 'disabled');
+    }
+
+    else{
+      $('#resetButton').removeAttr('disabled');
+    }
+  };
+  disableButton();
+
   $('#button').click(function(e){
     e.preventDefault();
     var toAdd= $('input[name=user_input]').val().trim();
     var addContent= $('<div class="add"><a href="#">&times</a><span>' +toAdd+'</span></div>');
     $('p').remove();
     $('#user_input').focus();  
-    
-    var displayErrorMessage = function(text) {
-      $('<p>'+ text +'</p>')
-        .hide()
-        .insertAfter($('#resetButton'))
-        .show(200);
-    };
+
 
     if(toAdd.length === 0){
       displayErrorMessage('Please write a message in the input field.');
-      return;
-    }
-
-    if($('.add').length === max_input){
-      displayErrorMessage('You have reached maximum capacity. Remove a label to add a new message'); 
       return;
     }
 
@@ -42,13 +59,17 @@ $(document).ready(function(){
       .hide()
       .appendTo($('.add_input'))
       .show(200);
-    $('#user_input').val('');  
+    $('#user_input').val(''); 
+
+    disableButton();
+
   });
        
   $('.add_input').on('click', 'a', function(e){
     e.preventDefault();
     $(this).closest('.add').hide(300, function(){
       $(this).remove();
+      disableButton();
     });
   });
 
@@ -56,9 +77,10 @@ $(document).ready(function(){
     e.preventDefault();
     $('.add').hide(300, function(){
       $(this).remove();
+      disableButton();
     });
   });
-
+ 
 });
 
  
