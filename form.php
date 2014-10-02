9dae3f63d5358efc54d60e65123560408124997e
saftaclaudia <saftaclaudia27@yahoo.com>
2014-10-02 19:00:22 +0300

    chanhe the script scc

--- form.php
+++ form.php
@@ -4,7 +4,7 @@
 <html>
   <head>
     <link rel='stylesheet' type='text/css' href='stylesheet.css'/>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script type='text/javascript' src="script.js"> </script>
   </head>
   <body>
