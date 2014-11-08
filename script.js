b23d951cc1517ce062813805ffc1207b9179d6bd
saftaclaudia <saftaclaudia27@yahoo.com>
2014-11-09 00:07:12 +0200

    refactoring labelsComponent function
    add init and attachEvents functions. Create return.

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
-var labelsComponents = function(idInput){
+var labelsComponents = function(idInput) {
+
 	var max_input = 6;
-	
-	var $input = $('#'+idInput);
-	$input.wrap('<div class="container"></div>');
-	var $container = $input.closest('.container');
-	
-	$container
-		.append('<input type="submit" value="Add '+idInput+'" class="button add_js">')
-		.append('<input type="reset" value="Remove '+idInput+'" class="button reset_js">')
-		.append('<div class="label_container"></div>');
-	
-	var $buttonAdd = $container.find('.add_js');
-	var $buttonReset = $container.find('.reset_js');
+	var $input, $container, $buttonAdd, $buttonReset;
 
+	var init = function() {
+		$input = $('#'+idInput);
+		$input.wrap('<div class="container"></div>');
+		$container = $input.closest('.container');
 
+		$container
+			.append('<input type="submit" value="Add '+idInput+'" class="button js-add">')
+			.append('<input type="reset" value="Remove All" class="button js-reset">')
+			.append('<div class="label_container"></div>');
+
+		$buttonAdd = $container.find('.js-add');
+		$buttonReset = $container.find('.js-reset');
+
+		toggleDisableElements();
+		attachEvents();
+	};
 
 	var isDuplicate = function( string ) {
 		var isCondition = false;
 		$container.find('.label').each(function( index, element ) {
 			if ( $(element).find('span').text() === string ) {
-				isCondition = true; 
+				isCondition = true;
 			}
 		});
 		return isCondition;
@@ -37,34 +42,24 @@ var labelsComponents = function(idInput){
 		$('#' + id).remove();
 	};
 
-	$input.on('input', function( e ) {
-		removeErrorMessage('duplicate_err');
-		if ( isDuplicate($(this).val()) === true ) {
-			displayErrorMessage('You already written this message. Please write another message.', 'duplicate_err');
-		}
-
-		if ( $(this).val().trim().length !== 0 ) {
-			removeErrorMessage('empty_err');
-		}
-	});
-
 	var toggleDisableElements = function() {
 		var nrLabels = $container.find('.label').length;
 
 		$buttonAdd.removeAttr('disabled');
 		$input.removeAttr('disabled');
 		$('p').remove();
+
 		if ( nrLabels >= max_input ) {
 			$buttonAdd.attr('disabled', 'disabled');
 			$input.attr('disabled', 'disabled');
 			displayErrorMessage( 'You have reached maximum capacity. Remove a label to add a new message' , 'max_err');
 		}
+
 		$buttonReset.removeAttr('disabled');
 		if ( nrLabels === 0 ) {
 			$buttonReset.attr('disabled', 'disabled');
 		}
 	};
-	toggleDisableElements();
 
 	var addNewLabel = function(value) {
 		var addContent = $('<div class="label"><a href="#">&times</a><span>' +value+'</span></div>');
@@ -90,56 +85,86 @@ var labelsComponents = function(idInput){
 		toggleDisableElements();
 	};
 
-	$buttonAdd.click(function( e ) {
-		e.preventDefault();
-		var toAdd = $input.val().trim();
-		$input.val(''); 
-	    addNewLabel( toAdd );
-		toggleDisableElements();
-	});
+	var attachEvents = function() {
+		$input.on('input', function( e ) {
+			removeErrorMessage('duplicate_err');
+			if ( isDuplicate($(this).val()) === true ) {
+				displayErrorMessage('You already written this message. Please write another message.', 'duplicate_err');
+			}
 
-	$container.find('.label_container').on('click', 'a', function( e ) {
-		e.preventDefault();
-		$(this).closest('.label').hide(300, function() {
-			$(this).remove();
-			toggleDisableElements();
+			if ( $(this).val().trim().length !== 0 ) {
+				removeErrorMessage('empty_err');
+			}
 		});
-	});
 
-	$buttonReset.click(function(e) {
-		e.preventDefault();
-		$('.label').hide(300, function() {
-			$(this).remove();
-			toggleDisableElements();
+		$buttonAdd.click(function( e ) {
+			e.preventDefault();
+			var toAdd = $input.val().trim();
+			addNewLabel( toAdd );
+			$input.val('');
 		});
-	});
 
-	var change_input = function(){
+		$container.find('.label_container').on('click', 'a', function( e ) {
+			e.preventDefault();
+			$(this).closest('.label').hide(300, function() {
+				$(this).remove();
+				toggleDisableElements();
+			});
+		});
+
+		$buttonReset.click(function(e) {
+			e.preventDefault();
+			$container.find('.label').hide(300, function() {
+				$(this).remove();
+				toggleDisableElements();
+			});
+		});
+
 		$container.find('.label_container').on('dblclick', '.label', function() {
 			var firstText = $(this).find('span').text();
-			$(this).find('span').replaceWith('<input type="text" data-first-text="' +firstText+ '"  name="edit_text" id="edit_text" value="' +firstText+'">');
-			$('#edit_text').focus();
+			$(this).find('span').replaceWith('<input type="text" data-first-text="' +firstText+ '" name="edit_text" value="' +firstText+'">');
+			$container.find('[name="edit_text"]').focus();
 		});
 
 		$container.find('.label_container').on('focusout', '.label', function() {
-			var newText = $('#edit_text').val().trim();
+			var $inputEdit = $('[name="edit_text"]'),
+				newText = $inputEdit.val().trim();
+
 			if ( isDuplicate(newText) === true || newText === '' ){
 				displayErrorMessage('You already written this message. Please write another message.', 'duplicate_err');
-				$('#edit_text').replaceWith('<span>' + $('#edit_text').attr('data-first-text') + '</span>');
-				return;
-			}
-			else{
-				$('#edit_text').replaceWith('<span>' + newText+ '</span>');
+				$inputEdit.replaceWith('<span>' + $inputEdit.attr('data-first-text') + '</span>');
+			} else {
+				$inputEdit.replaceWith('<span>' + newText+ '</span>');
 				$input.focus();
 			}
 		});
-	}
-	change_input();
+	};
+
+	init();
+
+	return {
+		getNumber: function(){
+			return $container.find('.labels').length;
+		},
+		addLabel: function(value){
+			addNewLabel(value);
+		},
+		maxInput: function(value){
+			max_input = value;
+			toggleDisableElements();
+		}
+	};
 };
 
 
-
 $(document).ready(function() {
-	labelsComponents('labels');
-	labelsComponents('hobbies');
+	var labels = labelsComponents('labels');
+	var hobbies = labelsComponents('hobbies');
+	console.log(labels.getNumber);
+	console.log(hobbies.getNumber);
+	labels.addLabel('food');
+	hobbies.addLabel('painting');
+	labels.maxInput(10);
+	hobbies.maxInput(10);
+
 });
